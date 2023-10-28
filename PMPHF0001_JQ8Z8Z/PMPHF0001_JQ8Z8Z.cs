5e8b160321a2b4e09e2a0be7877da33f36f0f85a
Rille Soma <rillesoma75@gmail.com>
2023-10-28 12:50:49 +0200

    upload

---
+++ PMPHF0001_JQ8Z8Z/PMPHF0001_JQ8Z8Z.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMPHF0001_JQ8Z8Z
+{
+    internal class PMPHF0001_JQ8Z8Z
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MinValue;
+            int x = int.Parse(Console.ReadLine());
+            if (x > max)
+            {
+                max = x;
+            }
+            int y = int.Parse(Console.ReadLine());
+            if (y > max)
+            {
+                max = y;
+            }
+            int z = int.Parse(Console.ReadLine());
+            if (z > max)
+            {
+                max = z;
+            }
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
